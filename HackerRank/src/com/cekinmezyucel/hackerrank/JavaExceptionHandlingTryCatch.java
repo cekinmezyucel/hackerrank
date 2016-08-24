a55659f0458072ff1fe29b44f307b0f1775cd238
Yucel Cekinmez <cekinmezyucel>
2016-08-24 11:48:32 +0300

    code formatting

--- HackerRank/src/com/cekinmezyucel/hackerrank/JavaExceptionHandlingTryCatch.java
+++ HackerRank/src/com/cekinmezyucel/hackerrank/JavaExceptionHandlingTryCatch.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 
 public class JavaExceptionHandlingTryCatch {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-            
-            try{
-                int x = scan.nextInt();
-                int y = scan.nextInt();
-                int z = x/y;
-                System.out.println(z);
-            } catch (InputMismatchException e1){
-                System.out.println("java.util.InputMismatchException");  
-            } catch (ArithmeticException e2){
-                System.out.println(e2);
-            } finally {
-				scan.close();
-			}
-    }
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		try {
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+			int z = x / y;
+			System.out.println(z);
+		} catch (InputMismatchException e1) {
+			System.out.println("java.util.InputMismatchException");
+		} catch (ArithmeticException e2) {
+			System.out.println(e2);
+		} finally {
+			scan.close();
+		}
+	}
 }

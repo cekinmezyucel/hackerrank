13b0bb358eb2afd201941b85945f09cb2cb15ade
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-08-24 21:01:12 +0300

    Java Regex

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/java/strings/JavaRegex.java
@@ -0,0 +1,12 @@
+package com.cekinmezyucel.hackerrank.java.strings;
+
+public class JavaRegex {
+
+	// Required code!!
+	class myRegex {
+		public static final String pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+				+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+				+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+	}
+
+}

52f9a1823d76f3dc6598d8cc533f54cc01c7add7
Yucel Cekinmez <cekinmezyucel>
2016-08-24 11:35:38 +0300

    Exception handling

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/Solution.java
@@ -0,0 +1,22 @@
+package com.cekinmezyucel.hackerrank;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+            
+            try{
+                int x = scan.nextInt();
+                int y = scan.nextInt();
+                int z = x/y;
+                System.out.println(z);
+            } catch (InputMismatchException e1){
+                System.out.println("java.util.InputMismatchException");  
+            } catch (ArithmeticException e2){
+                System.out.println(e2);
+            } 
+    }
+}

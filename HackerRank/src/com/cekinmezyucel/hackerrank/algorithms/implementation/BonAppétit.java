ef45fe4a936b408d5a56041bce6b7cd9f168fa6a
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-10-01 12:13:14 +0300

    Bon Appétit

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/algorithms/implementation/BonAppétit.java
@@ -0,0 +1,33 @@
+package com.cekinmezyucel.hackerrank.algorithms.implementation;
+
+import java.util.Scanner;
+
+public class BonAppétit {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		int anna = 0;
+		int annaBill = 0;
+
+		for (int i = 0; i < n; i++) {
+			int price = scan.nextInt();
+			if (!(i == k)) {
+				anna += price;
+			}
+		}
+
+		annaBill = scan.nextInt();
+
+		if (anna / 2 == annaBill) {
+			System.out.println("Bon Appetit");
+		} else {
+			System.out.println(annaBill - anna / 2);
+		}
+
+		scan.close();
+	}
+
+}

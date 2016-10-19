ccb017bbf6f91812dec2614010dad142ac826c70
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-10-19 21:31:03 +0300

    Print the Elements of a Linked List

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/datastructures/linkedlists/PrintTheElementsOfALinkedList.java
@@ -0,0 +1,61 @@
+package com.cekinmezyucel.hackerrank.datastructures.linkedlists;
+
+import java.util.Scanner;
+
+class Node {
+	int data;
+	Node next;
+}
+
+public class PrintTheElementsOfALinkedList {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int testcases = scan.nextInt();
+
+		for (int i = 0; i < testcases; i++) {
+			int nodeCount = scan.nextInt();
+
+			Node head = null;
+			Node temp = null;
+			for (int j = 0; j < nodeCount; j++) {
+				Node node = new Node();
+				node.data = scan.nextInt();
+
+				if (temp != null) {
+					temp.next = node;
+				} else {
+					head = node;
+				}
+
+				temp = node;
+			}
+
+			Print(head);
+		}
+		scan.close();
+	}
+
+	// This is the code without static keyword
+	static void Print(Node head) {
+		if (null != head) {
+			if (null != head.next) {
+				System.out.println(head.data);
+				Print(head.next);
+			} else if (null == head.next) {
+				System.out.println(head.data);
+			}
+		}
+	}
+}
+
+// TestCase
+// 5
+// 0
+// 2
+// 1 2
+// 4
+// 2 1 4 5
+// 3
+// 34 45 56
+// 5
+// 1 2 3 4 5

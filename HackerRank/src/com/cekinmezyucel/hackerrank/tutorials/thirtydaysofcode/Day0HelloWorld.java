6c80e850e407cdb3cb47f86520a41d88fcceaf0f
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-10-31 09:22:47 +0200

    Day 0: Hello, World.

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/tutorials/thirtydaysofcode/Day0HelloWorld.java
@@ -0,0 +1,18 @@
+package com.cekinmezyucel.hackerrank.tutorials.thirtydaysofcode;
+
+import java.util.Scanner;
+
+public class Day0HelloWorld {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String inputString = scan.nextLine();
+		scan.close();
+
+		System.out.println("Hello, World.");
+
+		System.out.println(inputString);
+
+	}
+
+}

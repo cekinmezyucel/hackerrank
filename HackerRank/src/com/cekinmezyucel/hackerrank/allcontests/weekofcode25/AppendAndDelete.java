6caa2742b8af6a1ba3e1081d9ecb33efdcc7c433
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-11-01 13:07:15 +0200

    Append and Delete

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/allcontests/weekofcode25/AppendAndDelete.java
@@ -0,0 +1,51 @@
+package com.cekinmezyucel.hackerrank.allcontests.weekofcode25;
+
+import java.util.Scanner;
+
+public class AppendAndDelete {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		String t = in.next();
+		int k = in.nextInt();
+
+		int same = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (t.length() > i && s.charAt(i) == t.charAt(i)) {
+				same++;
+			} else {
+				break;
+			}
+		}
+		int sOperation = s.substring(same).length();
+		int tOperation = t.substring(same).length();
+
+		printResult(s, t, k, sOperation, tOperation);
+		in.close();
+
+	}
+
+	static void printResult(String s, String t, int k, int sOperation, int tOperation) {
+		if (s.equals(t)) {
+			if (k >= s.length()) {
+				System.out.println("Yes");
+				return;
+			}
+		}
+
+		if (sOperation < s.length()) {
+			if (((k - (sOperation + tOperation)) % 2 == 0) && (k - (sOperation + tOperation)) >= 0) {
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+		} else {
+			if ((k - (sOperation + tOperation)) >= 0) {
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+		}
+	}
+}

4a22ea9c4342e7966bb58b29ff2d4eac44d6f1c7
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-11-03 16:05:16 +0200

    bak

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/allcontests/weekofcode25/StoneDivision.java
@@ -0,0 +1,48 @@
+package com.cekinmezyucel.hackerrank.allcontests.weekofcode25;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StoneDivision {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		long n = scan.nextLong();
+		int m = scan.nextInt();
+		long si[] = new long[m];
+
+		for (int i = 0; i < m; i++) {
+			si[i] = scan.nextLong();
+		}
+
+		Arrays.sort(si);
+
+		if (calculateWin(n, m, si, si.length - 1)) {
+			System.out.println("First");
+		} else {
+			System.out.println("Second");
+		}
+	}
+
+	static boolean calculateWin(long n, int m, long[] si, int siIndex) {
+
+		long siN = si[siIndex];
+
+		if (n / siN == 0)
+			return true;
+
+		if (n % siN == 0) {
+			long firstPile = n / siN;
+			if (firstPile % 2 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		return calculateWin(n, m, si, siIndex - 1);
+
+	}
+
+}

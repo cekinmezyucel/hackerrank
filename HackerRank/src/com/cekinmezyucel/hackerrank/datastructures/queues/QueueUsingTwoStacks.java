0b65e2145716b0a3b203fb490fe1b440bcac57e5
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-11-21 13:22:21 +0200

    Queue using Two Stacks

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/datastructures/queues/QueueUsingTwoStacks.java
@@ -0,0 +1,64 @@
+package com.cekinmezyucel.hackerrank.datastructures.queues;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class QueueUsingTwoStacks {
+
+	public static class MyQueue<T> {
+		Stack<T> stackNewestOnTop = new Stack<T>();
+		Stack<T> stackOldestOnTop = new Stack<T>();
+
+		public void enqueue(T value) { // Push onto newest stack
+			stackNewestOnTop.push(value);
+		}
+
+		public T peek() {
+			if (stackOldestOnTop.isEmpty()) {
+				if (stackNewestOnTop.size() == 1) {
+					return stackNewestOnTop.peek();
+				} else {
+					fillOldestOnTop();
+				}
+			}
+			return stackOldestOnTop.peek();
+		}
+
+		public T dequeue() {
+			if (stackOldestOnTop.isEmpty()) {
+				if (stackNewestOnTop.size() == 1) {
+					return stackNewestOnTop.pop();
+				} else {
+					fillOldestOnTop();
+				}
+			}
+			return stackOldestOnTop.pop();
+		}
+
+		void fillOldestOnTop() {
+			while (!stackNewestOnTop.isEmpty()) {
+				stackOldestOnTop.push(stackNewestOnTop.pop());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		MyQueue<Integer> queue = new MyQueue<Integer>();
+
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int operation = scan.nextInt();
+			if (operation == 1) { // enqueue
+				queue.enqueue(scan.nextInt());
+			} else if (operation == 2) { // dequeue
+				queue.dequeue();
+			} else if (operation == 3) { // print/peek
+				System.out.println(queue.peek());
+			}
+		}
+		scan.close();
+	}
+
+}

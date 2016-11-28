603bf6f2742bbe679e3cd35c92a446dbdf79f0ed
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-11-28 15:42:24 +0200

    Day 17: More Exceptions

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/tutorials/thirtydaysofcode/Day17MoreExceptions.java
@@ -0,0 +1,36 @@
+package com.cekinmezyucel.hackerrank.tutorials.thirtydaysofcode;
+
+import java.util.Scanner;
+
+class Calculator {
+
+	public int power(int n, int p) throws Exception {
+		if (n < 0 || p < 0) {
+			throw new Exception("n and p should be non-negative");
+		}
+
+		return (int) Math.pow(n, p);
+	}
+}
+
+public class Day17MoreExceptions {
+
+	public static void main(String[] argh) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		while (T-- > 0) {
+			int n = in.nextInt();
+			int p = in.nextInt();
+			Calculator myCalculator = new Calculator();
+			try {
+				int ans = myCalculator.power(n, p);
+				System.out.println(ans);
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+	}
+
+}

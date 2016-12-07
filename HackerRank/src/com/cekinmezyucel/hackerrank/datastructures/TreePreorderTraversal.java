0cfcae01797e912a1a063e5d65885a2bc90a24a3
Yucel Cekinmez <cekinmezyucel@gmail.com>
2016-12-07 14:42:22 +0200

    Tree: Preorder Traversal

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/datastructures/TreePreorderTraversal.java
@@ -0,0 +1,58 @@
+package com.cekinmezyucel.hackerrank.datastructures;
+
+import java.util.Scanner;
+
+class TreeNode {
+	TreeNode left, right;
+	int data;
+
+	TreeNode(int data) {
+		this.data = data;
+		left = right = null;
+	}
+}
+
+public class TreePreorderTraversal {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		TreeNode root = null;
+		while (T-- > 0) {
+			int data = sc.nextInt();
+			root = insert(root, data);
+		}
+		sc.close();
+		preOrder(root);
+	}
+
+	public static void preOrder(TreeNode root) {
+
+		if (null == root) {
+			return;
+		}
+
+		System.out.printf(root.data + " ");
+
+		preOrder(root.left);
+		preOrder(root.right);
+
+	}
+
+	public static TreeNode insert(TreeNode root, int data) {
+		if (root == null) {
+			return new TreeNode(data);
+		} else {
+			TreeNode cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+}

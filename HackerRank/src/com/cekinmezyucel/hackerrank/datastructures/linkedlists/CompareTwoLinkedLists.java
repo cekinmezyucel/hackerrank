28064a0fd2407979114f54bda118ba422f3df5d5
Yucel Cekinmez <cekinmezyucel@gmail.com>
2017-01-03 22:12:35 +0300

    Compare two linked lists

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/datastructures/linkedlists/CompareTwoLinkedLists.java
@@ -0,0 +1,22 @@
+package com.cekinmezyucel.hackerrank.datastructures.linkedlists;
+
+public class CompareTwoLinkedLists {
+	public static void main(String[] args) {
+		Node headA = new Node(1, new Node(2, new Node(3, new Node(4, null))));
+		Node headB = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5, null)))));
+
+		System.out.println(CompareLists(headA, headB));
+	}
+
+	static int CompareLists(Node headA, Node headB) {
+		if (null == headA && null == headB) {
+			return 1;
+		} else if (null == headA || null == headB) {
+			return 0;
+		} else if (headA.data != headB.data) {
+			return 0;
+		} else {
+			return CompareLists(headA.next, headB.next);
+		}
+	}
+}

4505ea78fec74d6ffbfbc12c65726b491da90ac9
Yucel Cekinmez <cekinmezyucel@gmail.com>
2017-01-19 10:07:37 +0300

    Bot saves princess - 2 refactor code

--- HackerRank/src/com/cekinmezyucel/hackerrank/artificialintelligence/botbuilding/BotSavesPrincess2.java
+++ HackerRank/src/com/cekinmezyucel/hackerrank/artificialintelligence/botbuilding/BotSavesPrincess2.java
@@ -5,54 +5,53 @@ import java.util.Scanner;
 public class BotSavesPrincess2 {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int m = scan.nextInt();
-		int r = scan.nextInt();
-		int c = scan.nextInt();
-		int mX = -1;
-		int mY = -1;
+		Scanner in = new Scanner(System.in);
+		int n, r, c;
+		n = in.nextInt();
+		r = in.nextInt();
+		c = in.nextInt();
+		String grid[] = new String[n];
+
+		for (int i = 0; i < n; i++) {
+			grid[i] = in.next();
+		}
+
+		in.close();
+		nextMove(n, r, c, grid);
+	}
+
+	static void nextMove(int n, int r, int c, String[] grid) {
+
 		int pX = -1;
 		int pY = -1;
 
-		String grid[] = new String[m];
-		for (int i = 0; i < m; i++) {
-			grid[i] = scan.next();
+		for (int i = 0; i < n; i++) {
 			char[] chars = grid[i].toCharArray();
-			for (int j = 0; j < m; j++) {
-				if (chars[j] == 'm') {
-					mX = i;
-					mY = j;
-				} else if (chars[j] == 'p') {
+			for (int j = 0; j < n; j++) {
+				if (chars[j] == 'p') {
 					pX = i;
 					pY = j;
 				}
 			}
 		}
 
-		int difX = mX - pX;
-		int difY = mY - pY;
-		int absDifX = Math.abs(difX);
-		int absDifY = Math.abs(difY);
+		int difX = r - pX;
+		int difY = c - pY;
 
 		if (difX < 0) {
-			while (absDifX-- > 0) {
-				System.out.println("DOWN");
-			}
+			System.out.println("DOWN");
+			return;
 		} else if (difX > 0) {
-			while (absDifX-- > 0) {
-				System.out.println("UP");
-			}
+			System.out.println("UP");
+			return;
 		}
 
 		if (difY < 0) {
-			while (absDifY-- > 0) {
-				System.out.println("RIGHT");
-			}
+			System.out.println("RIGHT");
+			return;
 		} else if (difY > 0) {
-			while (absDifY-- > 0) {
-				System.out.println("LEFT");
-			}
+			System.out.println("LEFT");
+			return;
 		}
-
 	}
 }

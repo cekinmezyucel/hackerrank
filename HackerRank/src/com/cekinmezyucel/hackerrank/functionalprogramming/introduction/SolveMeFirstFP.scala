0476937a4388c3594f70c3e39bd1f161d9132238
Yucel Cekinmez <cekinmezyucel@gmail.com>
2017-01-20 22:44:08 +0300

    Solve Me First FP

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/functionalprogramming/introduction/SolveMeFirstFP.scala
@@ -0,0 +1,8 @@
+package com.cekinmezyucel.hackerrank.functionalprogramming.introduction
+
+object SolveMeFirstFP {
+  
+      def main(args: Array[String]) {
+        println(io.Source.stdin.getLines().take(2).map(_.toInt).sum)
+    }
+}

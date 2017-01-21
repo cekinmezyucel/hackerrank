a285129b7e7c90d47df921314068266007e1fec5
Yucel Cekinmez <cekinmezyucel@gmail.com>
2017-01-21 20:04:39 +0300

    Hello World

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/functionalprogramming/introduction/HelloWorld.scala
@@ -0,0 +1,10 @@
+package com.cekinmezyucel.hackerrank.functionalprogramming.introduction
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    f()
+  }
+  
+  def f() = println("Hello World")
+
+}

fa5c2d911fed5b85d6a3ac8f3c3c28f4d4a70be3
Yucel Cekinmez <cekinmezyucel@gmail.com>
2017-01-23 08:57:02 +0300

    refactor

--- HackerRank/src/com/cekinmezyucel/hackerrank/functionalprogramming/introduction/SolveMeFirstFP.scala
+++ HackerRank/src/com/cekinmezyucel/hackerrank/functionalprogramming/introduction/SolveMeFirstFP.scala
@@ -3,6 +3,6 @@ package com.cekinmezyucel.hackerrank.functionalprogramming.introduction
 object SolveMeFirstFP {
   
       def main(args: Array[String]) {
-        println(io.Source.stdin.getLines().take(2).map(_.toInt).sum)
+        println(io.Source.stdin.getLines().take(2).map { x => x.toInt }.sum)
     }
 }

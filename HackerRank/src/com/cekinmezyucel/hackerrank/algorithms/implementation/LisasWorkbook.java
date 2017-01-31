375d416a3bdb0baf870cc4e0146566bbe29fc124
Yucel Cekinmez <cekinmezyucel@gmail.com>
2017-01-31 10:45:37 +0300

    Lisa's Workbook

---
+++ HackerRank/src/com/cekinmezyucel/hackerrank/algorithms/implementation/LisasWorkbook.java
@@ -0,0 +1,37 @@
+package com.cekinmezyucel.hackerrank.algorithms.implementation;
+
+import java.util.Scanner;
+
+public class LisasWorkbook {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int chapter = in.nextInt();
+		int maxQ = in.nextInt();
+		int specialQ = 0;
+		int page = 1;
+
+		while (chapter-- > 0) {
+			int questions = in.nextInt();
+
+			for (int i = 1; i <= questions; i++) {
+
+				if (i == page) {
+					specialQ++;
+				}
+
+				if (i % maxQ == 0) {
+					page++;
+				}
+
+			}
+
+			if (questions % maxQ != 0) {
+				page++;
+			}
+		}
+		in.close();
+		System.out.println(specialQ);
+	}
+
+}
